75b49bf856ac8977f470bac6baa517df45c86d3b
Alnezis <alnezis@ya.ru>
2020-01-02 19:30:54 +0300

    Update object PrivateMessage for VK API 5.103

--- vk/object/PrivateMessage.go
+++ vk/object/PrivateMessage.go
@@ -26,6 +26,25 @@ type MessageAction struct {
 }
 
 type PrivateMessage struct {
+	Message    *Message    `json:"message" map:"message"`
+	ClientInfo *ClientInfo `json:"client_info" map:"client_info"`
+}
+
+type ClientInfo struct {
+	//"button_actions": [
+	//　"text",
+	//　"vkpay",
+	//　"open_app",
+	//　"location",
+	//　"open_link"
+	//],
+	Keyboard       bool `json:"keyboard" map:"keyboard"`
+	InlineKeyboard bool `json:"inline_keyboard" map:"inline_keyboard"`
+	Carousel       bool `json:"carousel" map:"carousel"`
+	//"lang_id": 0
+}
+
+type Message struct {
 	ID                float64           `json:"id" map:"id"`
 	Date              float64           `json:"date" map:"date"`
 	PeerID            float64           `json:"peer_id" map:"peer_id"`
@@ -33,6 +52,8 @@ type PrivateMessage struct {
 	UserID            float64           `json:"from_id" map:"from_id"`
 	Text              string            `json:"text" map:"text"`
 	RandomID          float64           `json:"random_id" map:"random_id"`
+	Ref               string            `json:"ref" map:"ref"`
+	RefSource         string            `json:"ref_source" map:"ref_source"`
 	Attachments       []*Attachment     `json:"attachments" map:"attachments"`
 	Important         bool              `json:"important" map:"important"`
 	Geo               *Geo              `json:"geo" map:"geo"`
